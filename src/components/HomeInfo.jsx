f563abf0a6d902d63348213bd8399dad66c2db2c
Prakhar Porwal <prakhar2001porwal@gmail.com>
2023-12-27 21:08:20 +0530

    Email working fine

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -10,7 +10,7 @@ const HomeInfo = ({ currentStage }) => {
         <span className='font-semibold mx-2 text-white'>Prakhar</span>
         👋
         <br />
-        A Software Engineer from India <small>IN</small>
+        Aspiring Software Engineer from India <small>IN</small>
       </h1>
     );
 
